4c501c1e1320a4f3ffb10fba4082ff8cb123e458
BIKRAM DEY <133648903+BikramDeySDE@users.noreply.github.com>
2025-10-29 19:46:41 +0530

    updated the Feature to Borrowing a Book
    updated the Feature to Borrowing a Book

--- src/main/java/com/bikram/Digital_Library/services/TransactionService.java
+++ src/main/java/com/bikram/Digital_Library/services/TransactionService.java
@@ -1,9 +1,10 @@
 package com.bikram.Digital_Library.services;
 
+import com.bikram.Digital_Library.dtos.BorrowDto;
 import com.bikram.Digital_Library.entities.Book;
 
 public interface TransactionService {
 	
-	Book borrowBook(int userId, int bookId);
-	
+	Book borrowBook(BorrowDto borrowDto);
+		
 }
